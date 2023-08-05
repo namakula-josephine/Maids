73d1d7db16e4691c93d00475bc59a157fcceba38
CalvinKirabo <106669343+Calvinkirabo@users.noreply.github.com>
2023-08-05 22:52:03 +0300

    cal

--- lib/src/book_temp.dart
+++ lib/src/book_temp.dart
@@ -1,6 +1,6 @@
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
-import 'package:maidmatch_app/src/rotate_icon.dart';
 
 import '../utils/app_styles.dart';
 
@@ -30,8 +30,50 @@ class _bookpage1State extends State<bookpage1> {
         child: ListView(
           children: [
              
-             Gap(100),
-             RevolvingIconsWidget(),
+             Gap(50),
+            Container(
+              padding:  const EdgeInsets.symmetric(horizontal:25 , vertical: 45),
+              child: Column(
+                children: [
+                  Gap(10),
+                  Row(
+                    children: [
+                      ClipOval(
+                        child: Image.asset("assets/laundry.jpg",
+                        height: 80,
+                      
+                        ),
+                      ),
+                      Gap(130),
+                      Container(
+                        padding: const EdgeInsets.symmetric( vertical: 5),
+                        decoration: BoxDecoration(
+                        ),
+                        child: Column(
+                          children: [
+                            ClipOval(
+                              child: Image.asset("assets/clean.jpg",
+                                height: 80,),
+                            ),
+                            Text('Cleaning',style: Styles.headlineStyle5),
+                          ],
+                        ),
+                      ),
+
+                    ],
+                  ),
+                Container(
+                   child: Icon(
+                   EvaIcons.shoppingCart, // Specify the cart icon here
+                   size: 95, // Adjust the size as needed
+                   color: Colors.black, // Choose the desired color for the icon
+                   
+                 ),),
+                 
+                ],
+              ),
+            ),
+
           ],
         )),
     );
--- lib/utils/app_styles.dart
+++ lib/utils/app_styles.dart
@@ -13,4 +13,5 @@ class Styles{
   static TextStyle headlineStyle2 = TextStyle(fontSize: 21, color: textColor, fontWeight: FontWeight.bold);
   static TextStyle headlineStyle3 = TextStyle(fontSize: 17, color: Colors.grey.shade500, fontWeight: FontWeight.w500);
   static TextStyle headlineStyle4 = TextStyle(fontSize: 14, color: Colors.grey.shade500, fontWeight: FontWeight.w500);
+  static TextStyle headlineStyle5 = TextStyle(fontSize: 10, color: Colors.black, fontWeight: FontWeight.bold);
 }
