1255d61e640bd6719a5d5b0e93ebeca77291a955
CalvinKirabo <106669343+Calvinkirabo@users.noreply.github.com>
2023-08-12 09:38:06 +0300

    calvin

--- lib/src/laundry.dart
+++ lib/src/laundry.dart
@@ -1,4 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:date_field/date_field.dart';
+import 'package:dropdown_button2/dropdown_button2.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:maidmatch_app/comp/custom_button.dart';
+
+import '../utils/app_styles.dart';
 
 class Laundryform extends StatefulWidget {
   const Laundryform({super.key});
@@ -8,8 +16,164 @@ class Laundryform extends StatefulWidget {
 }
 
 class _LaundryformState extends State<Laundryform> {
+  final db = FirebaseFirestore.instance;
+  FirebaseDatabase database = FirebaseDatabase.instance;
+
+  final List<String> category = [
+  'Light-Many',
+  'Light-Few',
+  'Heavy-Many',
+  'Heavy-Few',
+  ];
+
+  String? selectedValue;
+  DateTime? selectedtime;
+  DateTime? selecteddate;
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return  Scaffold(
+      appBar: AppBar(
+          backgroundColor: Colors.orange,
+          title: Text(
+            "MAID MATCH",
+            textAlign: TextAlign.center,
+          ),
+        ),
+        backgroundColor: Styles.backgColor,
+        body: SafeArea(child: ListView(
+          padding: EdgeInsets.symmetric(horizontal: 20,),
+          children:[
+            Gap(20),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.orange.shade100
+              ),
+              child: Text("Laundry order here please!",
+              style: Styles.headlineStyle,
+              textAlign: TextAlign.center,
+              ),
+            ),
+            Gap(50),
+            DateTimeFormField(
+
+                decoration: const InputDecoration(
+                hintStyle: TextStyle(color: Colors.black45),
+                errorStyle: TextStyle(color: Colors.redAccent),
+                border: OutlineInputBorder(),
+                suffixIcon: Icon(Icons.event_note),
+                labelText: 'Request date',
+              ),
+                mode: DateTimeFieldPickerMode.date,
+                autovalidateMode: AutovalidateMode.always,
+                validator: (e) => (e?.day ?? 0) == 1 ? 'Please not the first day' : null,
+                onDateSelected: (DateTime value) {
+                  selectedtime = value;
+              },
+            ),
+            Gap(30),
+            DateTimeFormField(
+                decoration: const InputDecoration(
+                hintStyle: TextStyle(color: Colors.black45),
+                errorStyle: TextStyle(color: Colors.redAccent),
+                border: OutlineInputBorder(),
+                suffixIcon: Icon(Icons.event_note),
+                labelText: 'Request time',
+              ),
+                mode: DateTimeFieldPickerMode.time,
+                autovalidateMode: AutovalidateMode.always,
+                validator: (e) => (e?.day ?? 0) == 1 ? 'Please not the first day' : null,
+                onDateSelected: (DateTime value) {
+                  selecteddate = value;
+              },
+            ),
+            Gap(30),
+             DropdownButtonFormField2<String>(
+              isExpanded: true,
+              decoration: InputDecoration(
+                // Add Horizontal padding using menuItemStyleData.padding so it matches
+                // the menu padding when button's width is not specified.
+                contentPadding: const EdgeInsets.symmetric(vertical: 16),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                // Add more decoration..
+              ),
+              hint: const Text(
+                'Select your laundry category',
+                style: TextStyle(fontSize: 14),
+              ),
+              items: category
+                  .map((item) => DropdownMenuItem<String>(
+                        value: item,
+                        child: Text(
+                          item,
+                          style: const TextStyle(
+                            fontSize: 14,
+                          ),
+                        ),
+                      ))
+                  .toList(),
+              validator: (value) {
+                if (value == null) {
+                  return 'Please select category.';
+                }
+                return null;
+              },
+              onChanged: (value) {
+                //Do something when selected item is changed.
+              },
+              onSaved: (value) {
+                selectedValue = value.toString();
+              },
+              buttonStyleData: const ButtonStyleData(
+                padding: EdgeInsets.only(right: 8),
+              ),
+              iconStyleData: const IconStyleData(
+                icon: Icon(
+                  Icons.arrow_drop_down,
+                  color: Colors.black45,
+                ),
+                iconSize: 24,
+              ),
+              dropdownStyleData: DropdownStyleData(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+              ),
+              menuItemStyleData: const MenuItemStyleData(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+              ),
+            ),
+            Gap(30),
+            Container(
+              child: Text("calculated price", style: TextStyle(
+                color: Colors.black26,
+                fontSize: 15,
+              ),),
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.orange,
+                ),
+                borderRadius: BorderRadius.circular(10)
+              ),
+            ),
+            Gap(30),
+            customButton(
+              text: "submit",
+             onPressed: (){
+
+               /* Map<String, dynamic> data ={
+                  "category": selectedValue,
+                  "request_time": selectedtime,
+                  "request_date": selecteddate,
+                }; */
+              
+
+             })
+          ],
+        )),
+    );
   }
 }
